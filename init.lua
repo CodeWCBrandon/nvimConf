b589417e5c5192aee85e478ebacde816e481f020
CodeWCBrandon <christian.brandon@binus.ac.id>
2025-07-11 22:31:18 +0700

    changes for the mobile again

--- init.lua
+++ init.lua
@@ -4,3 +4,19 @@ require("config.lazy")
 -- using bashrc
 vim.opt.shell = "/bin/bash"
 vim.opt.shellcmdflag = "-ic" -- interactive shell, loads ~/.bashrc
+
+-- mobile compability
+if vim.fn.has("unix") == 1 then
+  vim.g.clipboard = {
+    name = "termux-clipboard",
+    copy = {
+      ["+"] = "termux-clipboard-set",
+      ["*"] = "termux-clipboard-set",
+    },
+    paste = {
+      ["+"] = "termux-clipboard-get",
+      ["*"] = "termux-clipboard-get",
+    },
+    cache_enabled = 0,
+  }
+end
