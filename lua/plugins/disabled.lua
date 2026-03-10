9bafab26b9609f9d1f52c88d96e2ef24959d97d2
CodeWCBrandon <christian.brandon@binus.ac.id>
2026-03-10 19:52:02 +0700

    update for disabling tree setter

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  -- This disables nvim-treesitter
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  { "nvim-treesitter-textobjects/nvim-treesitter-textobjects", enabled = false},
+}
